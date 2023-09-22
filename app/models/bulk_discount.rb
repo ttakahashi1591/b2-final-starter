1458af6a23e3ba5bb9113328bd17ce7e9d24dda1
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-09-22 15:39:05 -0600

    feat: Add BulkDiscount validations and model testing for both Merchant and BulkDisco unt

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,4 +1,7 @@
 class BulkDiscount < ApplicationRecord
+  validates :percentage_discount, presence: true
+  validates :quantity_threshold, presence: true
+  
   belongs_to :merchant
   has_many :items, through: :merchant
 end
--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 describe BulkDiscount do
-  # describe "validations" do
-  #   it { should validate_presence_of :name }
-  # end
+  describe "validations" do
+    it { should validate_presence_of :percentage_discount }
+    it { should validate_presence_of :quantity_threshold }
+  end
 
   describe "relationships" do
     it { should belong_to :merchant }

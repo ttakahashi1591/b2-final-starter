eae43dc5f282d326005d250d082f58750b0b5aa2
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-09-25 14:50:04 -0600

    feat: Add Welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :welcome, only: [:index]
+
   resources :merchants, only: [:show] do
     resources :dashboard, only: [:index]
     resources :items, except: [:destroy]
